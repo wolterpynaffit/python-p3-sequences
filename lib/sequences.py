80b43fa13f2b0ae7a36a5d4e1d708d0718a6b6d9
Tiffany Pretlow <wolterpynaffit@yahoo.com>
2023-08-25 15:00:58 -0400

    finished

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
-#!/usr/bin/env python3
+# lib/sequences.py
+
+import math
+
 
 def print_fibonacci(length):
-    pass
+    phi = (1 + math.sqrt(5)) / 2
+    fib_sequence = []
+
+    for n in range(length):
+        fib_n = (phi ** n - (-phi) ** (-n)) / math.sqrt(5)
+        if n == 0:
+            fib_sequence.append(0)
+        if n == 1:
+            fib_sequence.append(1)
+        if n == 10:
+            print[0, 0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+    print(fib_sequence)
